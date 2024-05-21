7672f9ea80db9d2a0b272a17ac2d1fceaf6826f0
VukP <vuk.papic@student.um.si>
2024-05-21 22:45:00 +0200

    Refactor saveSpot method in SpotService to handle location properly and improve error handling

--- lib/services/spot_service.dart
+++ lib/services/spot_service.dart
@@ -6,35 +6,49 @@ import 'dart:convert';
 import 'dart:io';
 
 class SpotService {
-  Future<void> saveSpot(String title, DateTime date, String location, String description, File imageFile) async {
-    final accessToken = await AuthService().getAccessToken();
-    final userId = await AuthService().getUserId();
-    String baseUrl = '$BASE_URL/users/$userId/spots';
+  Future<void> saveSpot(String title, DateTime date, String location,
+      String description, File imageFile) async {
+    try {
+      final accessToken = await AuthService().getAccessToken();
+      final userId = await AuthService().getUserId();
+      String baseUrl = '$BASE_URL/users/$userId/spots';
 
-    var request = http.MultipartRequest('POST', Uri.parse(baseUrl));
+      var request = http.MultipartRequest('POST', Uri.parse(baseUrl));
 
-    request.headers.addAll(<String, String>{
-      'Content-Type': 'multipart/form-data',
-      'Authorization': 'Bearer $accessToken',
-    });
+      request.headers.addAll(<String, String>{
+        'Content-Type': 'multipart/form-data',
+        'Authorization': 'Bearer $accessToken',
+      });
 
-    request.fields['title'] = title;
-    request.fields['date'] = date.toString();
-    request.fields['location'] = location;
-    request.fields['description'] = description;
+      request.fields['title'] = title;
+      request.fields['date'] = date.toString();
+      request.fields['location'] = location;
+      var locationParts = location.split(',');
 
-    request.files.add(await http.MultipartFile.fromPath(
-      'image',
-      imageFile.path,
-      contentType: MediaType('image', 'jpeg'),
-    ));
+      // var lat = locationParts[0].trim();
+      // var lng = locationParts[1].trim();
 
-    var response = await request.send();
+      request.fields['location'] = location;
+      // request.fields['lng'] = lng;
+      // print('Location: $lat, $lng');
+      request.fields['description'] = description;
 
-    if (response.statusCode == 200) {
-      print('Spot saved successfully');
-    } else {
-      throw Exception('Failed to save spot');
+      request.files.add(await http.MultipartFile.fromPath(
+        'image',
+        imageFile.path,
+        contentType: MediaType('image', 'jpeg'),
+      ));
+      print('Sending request...');
+      var response = await request.send();
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print('Spot saved successfully');
+      } else {
+        throw Exception(
+            'Failed to save spot, status code: ${response.statusCode}, ${await response.stream.bytesToString()}');
+      }
+    } catch (e) {
+      print('Error: $e');
     }
   }
 }

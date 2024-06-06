7e4b57050389d4320b86867e1d7b551a344ddf35
VukP <vuk.papic@student.um.si>
2024-06-06 21:46:31 +0200

    Refactor main.dart and login_screen.dart to improve user authentication flow

--- lib/main.dart
+++ lib/main.dart
@@ -13,50 +13,86 @@ import 'services/auth_service.dart';
 import 'screens/login_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final authService = AuthService();
+  final token = await authService.loadToken();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => UserModel()),
         ChangeNotifierProvider(create: (context) => LanguageProvider()),
       ],
-      child: MyApp(),
+      child: MyApp(isLoggedIn: token != null),
     ),
   );
 }
+
 class MyApp extends StatelessWidget {
   final AuthService _authService = AuthService();
+  final bool isLoggedIn;
+
+  MyApp({required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Fishing App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      localizationsDelegates: const [
-        
-        AppLocalizations.delegate, // Add this line
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', 'US'), // English
-        Locale('sr', 'RS'), // Serbian
-        Locale('sl', 'SI'), // Slovenian
-        Locale('cr', 'HR'), // Croatian
-      ],
-      locale: Provider.of<LanguageProvider>(context).currentLocale,
-      home: _authService.isLoggedIn == true
-          ? BottomNavigationExample()
-          : LoginScreen(),
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/home': (context) => BottomNavigationExample(),
-        '/register': (context) => RegistrationScreen()
+    return FutureBuilder(
+      future: _initializeUserData(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(child: Text('Error loading user data')),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            title: 'Flutter Fishing App',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', 'US'),
+              Locale('sr', 'RS'),
+              Locale('sl', 'SI'),
+              Locale('cr', 'HR'),
+            ],
+            locale: Provider.of<LanguageProvider>(context).currentLocale,
+            home: isLoggedIn ? BottomNavigationExample() : LoginScreen(),
+            routes: {
+              '/login': (context) => LoginScreen(),
+              '/home': (context) => BottomNavigationExample(),
+              '/register': (context) => RegistrationScreen()
+            },
+          );
+        }
       },
     );
+  }
+
+  Future<void> _initializeUserData(BuildContext context) async {
+    if (isLoggedIn) {
+      final userModel = Provider.of<UserModel>(context, listen: false);
+      final userData = await _authService.getUserData();
+      // Use a post-frame callback to update the user model after the build is complete
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        userModel.setUser(userData);
+      });
+    }
   }
 }

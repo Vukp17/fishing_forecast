deca08fe1e947a85dfcf58821a800d76bd6fe3cb
VukP <vuk.papic@student.um.si>
2024-07-04 20:14:11 +0200

    feat: Add sliding_up_panel dependency and implement SpotPreviewPanel widget

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -6,7 +6,6 @@ import '../services/weather_service.dart';
 import '../models/weather_model.dart';
 import 'package:intl/intl.dart';
 
-
 class WeatherScreen extends StatefulWidget {
   @override
   _WeatherScreenState createState() => _WeatherScreenState();
@@ -45,7 +44,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   List<DateTime> _generateWeekDates(DateTime selectedDate) {
-    final startOfWeek = selectedDate.subtract(Duration(days: selectedDate.weekday - 1));
+    final startOfWeek =
+        selectedDate.subtract(Duration(days: selectedDate.weekday - 1));
     return List.generate(7, (index) => startOfWeek.add(Duration(days: index)));
   }
 
@@ -61,7 +61,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: weekDates.map((date) {
-              bool isSelected = date.day == selectedDate.day && date.month == selectedDate.month && date.year == selectedDate.year;
+              bool isSelected = date.day == selectedDate.day &&
+                  date.month == selectedDate.month &&
+                  date.year == selectedDate.year;
               return GestureDetector(
                 onTap: () {
                   setState(() {
@@ -70,7 +72,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                   });
                 },
                 child: Container(
-                  padding: EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
                   decoration: BoxDecoration(
                     color: isSelected ? Colors.blue : Colors.transparent,
                     borderRadius: BorderRadius.circular(8.0),
@@ -88,8 +90,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
                 GestureDetector(
                   onTap: () => _selectDate(context),
                   child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
                       children: [
                         Icon(Icons.calendar_today, color: Colors.grey),
                         Text('Calendar', style: TextStyle(color: Colors.grey)),
@@ -108,6 +110,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
                 } else if (snapshot.hasError) {
                   print(snapshot.error);
                   return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                      child: Text('No weather data available.'));
                 } else {
                   final weather = snapshot.data!.first;
                   return Column(
@@ -115,32 +120,53 @@ class _WeatherScreenState extends State<WeatherScreen> {
                       Expanded(
                         child: ListView(
                           children: [
-                            _buildParameterCard('Temperature', weather.temperature, Icons.thermostat),
-                            _buildParameterCard('Wind Speed', weather.windSpeed, Icons.air),
-                            _buildParameterCard('Sunrise/Sunet', DateFormat.Hm().format(DateTime.parse(weather.sunrise)), Icons.wb_sunny),
-                            // _buildParameterCard('Sunset', DateFormat.Hm().format(DateTime.parse(weather.sunset)), Icons.nights_stay),
+                            _buildParameterCard('Temperature',
+                                weather.temperature, Icons.thermostat),
+                            _buildParameterCard(
+                                'Wind Speed', weather.windSpeed, Icons.air),
+                            _buildParameterCard(
+                                'Sunrise/Sunset',
+                                '${DateFormat.Hm().format(DateTime.parse(weather.sunrise))} / ${DateFormat.Hm().format(DateTime.parse(weather.sunset))}',
+                                Icons.wb_sunny),
                           ],
                         ),
                       ),
                       AnimatedSize(
-                        duration: Duration(milliseconds: 300),
-                        child: showGraph
+                        duration: const Duration(milliseconds: 300),
+                        child: selectedParameter == 'Sunrise/Sunset' &&
+                                !showGraph
                             ? Card(
-                                key: ValueKey<String>(selectedParameter),
+                                shadowColor: Colors.grey ,
+                                elevation: 5,
+                                color: Colors.white,
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
-                                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                                child: Container(
-                                  height: 300, // Adjusted height for the chart
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: WeatherChart(
-                                    data: snapshot.data!,
-                                    parameter: selectedParameter,
-                                  ),
-                                ),
-                              )
-                            : Container(), // Empty container when no graph is shown
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: SunriseSunsetWidget(
+                                  sunrise: weather.sunrise,
+                                  sunset: weather.sunset,
+                                ))
+                            : showGraph
+                                ? Card(
+                                    key: ValueKey<String>(selectedParameter),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 8),
+                                    child: Container(
+                                      height:
+                                          300, // Adjusted height for the chart
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: WeatherChart(
+                                        data: snapshot.data!,
+                                        parameter: selectedParameter,
+                                      ),
+                                    ),
+                                  )
+                                : Container(), // Empty container when no graph is shown
                       ),
                     ],
                   );
@@ -159,34 +185,37 @@ class _WeatherScreenState extends State<WeatherScreen> {
       onTap: () {
         setState(() {
           selectedParameter = parameter;
-          showGraph = true;
+          showGraph = parameter != 'Sunrise/Sunset';
         });
       },
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
-          side: isSelected ? BorderSide(color: Colors.blue, width: 2) : BorderSide.none,
+          side: isSelected
+              ? const BorderSide(color: Colors.blue, width: 2)
+              : BorderSide.none,
         ),
         color: Colors.white,
-        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Container(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Aligns the text to left and right
+            mainAxisAlignment: MainAxisAlignment
+                .spaceBetween, // Aligns the text to left and right
             children: [
               Row(
                 children: [
                   Icon(icon, size: 24.0),
-                  SizedBox(width: 8.0),
+                  const SizedBox(width: 8.0),
                   Text(
                     value.toString(),
-                    style: TextStyle(fontSize: 16.0),
+                    style: const TextStyle(fontSize: 16.0),
                   ),
                 ],
               ),
               Text(
                 parameter,
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ],
           ),
